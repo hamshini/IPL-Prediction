e084f0bb4d32a1d3f7acd6a3c611065479a4a775
hamshini <hamshinikkdi88@gmail.com>
2026-03-20 23:05:24 +0530

    updated cors with frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-// app.use(cors({
-//     origin: "http://localhost:3000"
-// }))
 app.use(cors({
-    origin: "*"
+    origin: [
+        "http://localhost:3000",
+        "https://ipl-prediction-frontend.vercel.app/"
+    ]
 }))
 
 app.get("/users", async (req, res) => {
